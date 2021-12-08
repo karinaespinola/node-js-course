a2333c2befa9b7bb4e885e68e97711ac7b355997
Karina Espinola <ke@jexan.com>
2021-12-07 18:40:41 -0600

    Use .pipe to stream data from a file

--- 02/stream.js
+++ 02/stream.js
@@ -4,6 +4,10 @@ const rs = fs.createReadStream('./files/file-2.txt', {encoding: 'utf8'});
 
 const ws = fs.createWriteStream('./files/stream-file-3.txt');
 
-rs.on('data', (dataChunk) => {
-  ws.write(dataChunk);
-});
+// rs.on('data', (dataChunk) => {
+//   ws.write(dataChunk);
+// });
+
+// Even a more efficient way to stream data:
+
+rs.pipe(ws);
