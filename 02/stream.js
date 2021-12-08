05f9659ed4da73506af4aaa7b4b71f95560c25d2
Karina Espinola <ke@jexan.com>
2021-12-07 18:36:14 -0600

    Stream data using a event

---
+++ 02/stream.js
@@ -0,0 +1,9 @@
+const fs = require('fs');
+
+const rs = fs.createReadStream('./files/file-2.txt', {encoding: 'utf8'});
+
+const ws = fs.createWriteStream('./files/stream-file-3.txt');
+
+rs.on('data', (dataChunk) => {
+  ws.write(dataChunk);
+});
