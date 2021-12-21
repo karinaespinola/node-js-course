f0a134799f5aff88b6d5b5b9b2af2f8516348cc5
Karina Espinola <k.espinolah@gmail.com>
2021-12-20 22:13:31 -0600

    Add some routes

--- 06/server.js
+++ 06/server.js
@@ -3,8 +3,16 @@ const app = express();
 const path = require('path');
 const PORT = process.env.PORT || 3500;
 
-app.get('/', (req, res) => {
-  res.send('Hello World!');
+// Express allows us to include regular expression in the route
+// In this case the route could be just a slash or include index.html
+// also .html is optional as well
+app.get('^/$|index(.html)?', (req, res) => {
+  // res.sendFile('./views/index.html', {root: __dirname});
+  res.sendFile(path.join(__dirname, 'views', 'index.html'))
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'))
 });
 
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
