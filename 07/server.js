49da29b7c561fc8e2d2460f65b843a5c33756a8e
Karina Espinola <k.espinolah@gmail.com>
2021-12-23 19:22:30 -0600

    Add initial middleware and create public folder

---
+++ 07/server.js
@@ -0,0 +1,58 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+const PORT = process.env.PORT || 3500;
+
+// Built-in middleware to handle urlencoded
+// in other words, form data;
+// 'CONTENT-TYPE: application/x-www-form-urlencoded'
+app.use(express.urlencoded({ extended: false }));
+
+// built-in middleware for json
+app.use(express.json());
+
+// serve static files
+app.use(express.static(path.join(__dirname, '/public')));
+
+// Express allows us to include regular expression in the route
+// In this case the route could be just a slash or include index.html
+// also .html is optional as well
+app.get('^/$|index(.html)?', (req, res) => {
+  // res.sendFile('./views/index.html', {root: __dirname});
+  res.sendFile(path.join(__dirname, 'views', 'index.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/new-page(.html)?', (req, res) => {
+  res.sendFile(path.join(__dirname, 'views', 'new-page.html'));
+});
+
+app.get('/old-page(.html)?', (req, res) => {
+  res.redirect(301, '/new-page.html'); // 302 by default
+});
+
+const one = (req, res, next) => {
+  console.log('One');
+  next();
+}
+
+const two = (req, res, next) => {
+  console.log('Two');
+  next();
+}
+
+const three = (req, res) => {
+  console.log('Three');
+  res.send("Finished!");
+}
+
+app.get('/chained(.html)?', [one, two, three]);
+
+app.get('/*', (req, res) => {
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
+})
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
