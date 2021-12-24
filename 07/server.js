90f338dbbd5255212363a2c2eeef58bd5375d3bf
Karina Espinola <k.espinolah@gmail.com>
2021-12-23 21:56:35 -0600

    Add CORS and whitelist and use it in middleware

--- 07/server.js
+++ 07/server.js
@@ -1,12 +1,26 @@
 const express = require('express');
 const app = express();
 const path = require('path');
+const cors = require('cors');
 const { logger } = require('./middleware/logEvents');
 const PORT = process.env.PORT || 3500;
 
 // Custom middleware to log requests
 app.use(logger);
-
+// Cross Origin Resource Sharing
+// Whitelist for development
+const whitelist = ['https://karinaespinola.dev'];
+const corsOptions = {
+  origin: (origin, callback) => {
+    if(whitelist.indexOf(origin) !== -1) {
+      callback(null, true);
+    } else {
+      callback(new Error('Not allowed by CORS'));
+    }
+  },
+  optionsSuccessStatus: 200
+}
+app.use(cors(corsOptions));
 
 // Built-in middleware to handle urlencoded
 // in other words, form data;
