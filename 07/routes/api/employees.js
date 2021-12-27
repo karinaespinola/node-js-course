aaaa5b889aaaad562a58e333deb9ea0f3d89cb54
Karina Espinola <k.espinolah@gmail.com>
2021-12-26 18:00:43 -0600

    Add the verifyJwt middleware to all /employees routes

--- 07/routes/api/employees.js
+++ 07/routes/api/employees.js
@@ -1,10 +1,9 @@
 const express = require('express');
 const router = express.Router();
 const employeesController = require('../../controllers/employeesController');
-const { verifyJwt } = require('../../middleware/verifyJwt');
 
 router.route('/')
-  .get(verifyJwt, employeesController.getAllEmployees)
+  .get(employeesController.getAllEmployees)
   .post(employeesController.createNewEmployee)
   .put(employeesController.updateEmployee)
   .delete(employeesController.deleteEmployee);
--- 07/server.js
+++ 07/server.js
@@ -5,6 +5,7 @@ const cors = require('cors');
 const corsOptions = require('./config/corsOptions');
 const { logger } = require('./middleware/logEvents');
 const errorHandler  = require('./middleware/errorHandler');
+const verifyJwt = require('./middleware/verifyJwt');
 const PORT = process.env.PORT || 3500;
 
 // Custom middleware to log requests
@@ -25,9 +26,11 @@ app.use('/subdir', express.static(path.join(__dirname, '/public')));
 
 app.use('/', require('./routes/root'));
 app.use('/subdir', require('./routes/subdir'));
-app.use('/employees', require('./routes/api/employees'));
 app.use('/register', require('./routes/api/register'));
 app.use('/auth', require('./routes/api/auth'));
+// Everything below this line will go through the verifyJwt middleware
+app.use(verifyJwt);
+app.use('/employees', require('./routes/api/employees'));
 
 app.get('/*', (req, res) => {
   res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
